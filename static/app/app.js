b6c02266dd019812049960c48a2e9cf1e318772a
bitdev01 <bitdev01@blueivytech.com>
2016-08-01 11:11:05 -0700

    routing updates.

--- static/app/app.js
+++ static/app/app.js
@@ -3,41 +3,34 @@ var naBaseApp = angular.module("naBaseApp", [ "ngRoute" ]);
 naBaseApp.config(function ($routeProvider) {
 	
     $routeProvider
-		.when( "/home",
-			   {
-				 controller: "HomeController",
-				 templateUrl: "/app/partials/home/home.html"
-			   }
-		)
-		.when( "/data/profiles", 
-		       { 
-				 controller: "DataProfileController", 
-		         templateUrl: "/app/partials/data/profiles.html" 
-	           }
-		)
-		.when( "/data/profiles/:profile_id",
-			   {
-				   controller: "DataProfileDetailController",
-				   templateUrl: "/app/partials/data/profileDetail.html"
-			   }
-		)
+		// non-public home
+		.when( "/home", {
+			controller: "HomeController",
+			templateUrl: "/app/partials/home/home.html"
+		})
+		
+		// data 
+		.when( "/data/profiles",{ 
+			controller: "DataProfileController", 
+		    templateUrl: "/app/partials/data/profiles.html" 
+	    })
+		.when( "/data/profiles/:profile_id",{
+			controller: "DataProfileDetailController",
+			templateUrl: "/app/partials/data/profileDetail.html"
+		})
 		
 		// route for vehicles
-		.when( "/data/vehicles",
-			   {
-				   controller: "DataVehicleController",
-				   templateUrl: "/app/partials/data/vehicles.html"
-			   }
-		)
+		.when( "/data/vehicles",{
+		    controller: "DataVehicleController",
+			templateUrl: "/app/partials/data/vehicles.html"
+		})
 		
 		// public/home
-		.when("/public/index",
-			  {
-				  //redirectTo: "/home"
-				  controller: "PublicController",
-				  templateUrl: "/app/views/public/home/index.html"
-			  }
-		)
+		.when("/public/index",{
+			//redirectTo: "/home"
+			controller: "PublicController",
+			templateUrl: "/app/views/public/home/index.html"
+		})
 		.when("/public/about/contact",{
 			controller: "PublicController",
 			templateUrl: "/app/views/public/home/aboutContact.html"
