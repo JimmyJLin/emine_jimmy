5e7bca191cd47d29a8651cd8719a010828b89091
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-10 15:56:07 -0400

    manually added add vehicle section due to merge conflict

--- app/angular-app/vehicle/vehicle_add/vehicleProvider.js
+++ app/angular-app/vehicle/vehicle_add/vehicleProvider.js
@@ -3,15 +3,17 @@
     function vehicleProvider ($http) {
 
       this.addVehicle = function(addVehicleData){
+        console.log(addVehicleData)
         $http.post('http://api.nationsauction.com/inventory/Vehicle/Add', addVehicleData)
           .then(function(response){
-            console.log("Vehicle posted!", response)
+            console.log('this is the response', response)
           })
           .catch(function(error){
             console.log("Unable to Add vehicle, error: ", error)
           })
 
       }
+
 
 
     }
