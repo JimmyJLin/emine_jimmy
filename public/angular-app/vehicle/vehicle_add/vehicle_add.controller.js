2c9782abad581f7e396d4319590f784064454a44
emineKoc <eminekoc@gmail.com>
2016-08-09 22:42:41 -0400

    add vehicle problem fixed

--- public/angular-app/vehicle/vehicle_add/vehicle_add.controller.js
+++ public/angular-app/vehicle/vehicle_add/vehicle_add.controller.js
@@ -2,24 +2,29 @@
 
 
 function VehiclesAddController($scope, vehicleProvider){
-  console.log(this)
-  let vm = this;
-  let addVehicleData = JSON.stringify({
-    'submitter': "submitter",
-    'vin': vm.vin,
-    'year': vm.year,
-    'make': vm.make,
-    'model': vm.model,
-    'type': vm.type,
-    'description': vm.description,
-    'price': vm.price,
-    'owner': vm.owner,
-    'activity': 'add'
-  })
+  var vm = this;
 
-  vehicleProvider.addVehicle(addVehicleData)
+  vm.addVehicle = function(){
+    var owner = "617a69c3-bd34-445c-a762-1bbcf228f7bb"
+
+      var addVehicleData = JSON.stringify({
+        'submitter': "submitter",
+        'vin': vm.vin,
+        'year': vm.year,
+        'make': vm.make,
+        'model': vm.model,
+        'type': vm.type,
+        'description': vm.description,
+        'price': vm.price,
+        'owner': owner,
+        'activity': 'add'
+      })
+
+
+  vehicleProvider.addVehicle(addVehicleData);
+
+      }
 }
-
-naBaseApp.controller("VehiclesAddController", ['$scope', 'vehicleProvider', VehiclesAddController]);
+  naBaseApp.controller("VehiclesAddController", ['$scope', 'vehicleProvider', VehiclesAddController]);
 
 })();
