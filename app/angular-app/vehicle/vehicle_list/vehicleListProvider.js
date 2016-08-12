5d002af770ec80a19c27816953156a792b5d70a0
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-11 21:24:37 -0400

    able to retrieve json file from backend, but unable to display the data on html page

--- app/angular-app/vehicle/vehicle_list/vehicleListProvider.js
+++ app/angular-app/vehicle/vehicle_list/vehicleListProvider.js
@@ -4,7 +4,7 @@
       var vm = this;
 
       this.getVehicleByOwnerID = function(vehicleListData){
-        console.log(vehicleListData)
+        // console.log(vehicleListData)
         $http({
           url: 'http://api.nationsauction.com/inventory/Vehicle/GetByOwner',
           method: 'POST',
@@ -15,8 +15,9 @@
         })
         // $http.post('http://api.nationsauction.com/inventory/Vehicle/GetByOwner', vehicleListData)
           .then(function(response){
-            console.log('this is the response', response.data)
-            vm.vehicles = response
+            vm.vehicles = response.data
+
+            console.log('this is the response', vm.vehicles)
           })
           .catch(function(error){
             console.log("Unable to get vehicle data, error: ", error)
